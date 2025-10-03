b8f24d72bae688a3094862483fd7d8041875c04e
Naughty000 <aroojabbas394@gmail.com>
2025-10-03 23:46:40 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,11 @@
+
+
+function validation()
+{
+    let n=document.querySelector("name");
+    let e=document.querySelector("email");
+    let p=document.querySelector("password");
+    let l_n=document.querySelector("last");
+    
+
+}
